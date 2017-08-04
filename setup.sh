399158d36e7ae71399fccd77e0a7d8b359ffe10e
Aaron Weiss <awe@pdgn.co>
2017-08-03 23:18:51 -0400

    Redid the setup script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 wd=$(pwd)
-ln -s $wd/bashrc $HOME/.bashrc
-ln -s $wd/vimrc $HOME/.vimrc
-ln -s $wd/vim $HOME/.vim
+
+# Set up git configurations.
 ln -s $wd/gitconfig $HOME/.gitconfig
 ln -s $wd/gitignore $HOME/.gitignore
+
+# Set up spacemacs.
+git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+ln -s $wd/spacemacs $HOME/.spacemacs
+
+# Set up zsh with oh-my-zsh.
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+ln -s $wd/zshrc $HOME/.zshrc
+ln -s $wd/zsh/*.zsh $HOME/.oh-my-zsh/custom/
+chsh -s /bin/zsh
+
+# Set up gpg-agent.
+ln -s $wd/gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+gpgconf --reload gpg-agent
