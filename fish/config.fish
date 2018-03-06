33b2368a98d8307424bbbdca69046a110963dc10
Aaron Weiss <awe@pdgn.co>
2018-03-06 17:05:29 +0000

    Reverted ssh-agent changes.

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,3 @@ set -x GPG_TTY (tty)
 set -x PATH $HOME/.cargo/bin $PATH
 # Add .local/bin to path.
 set -x PATH $HOME/.local/bin $PATH
-# Run ssh-agent if not on macOS.
-if [ (uname) != "Darwin" ]
-    eval (ssh-agent -c)
-    ssh-add
-end
