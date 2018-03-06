6f180e6561a4175830134fa1233dfab36539cdc9
Aaron Weiss <awe@pdgn.co>
2018-03-06 20:49:06 +0100

    Changed mkrepo/cprepo scripts to use GIT_SERVE_ROOT.

--- fish/functions/mkrepo.fish
+++ fish/functions/mkrepo.fish
@@ -1,12 +1,12 @@
 function mkrepo --description "create a new git repo"
   for name in $argv
-    mkdir /git/awe/$name.git
-    pushd /git/awe/$name.git
+    mkdir $GIT_SERVE_ROOT/$name.git
+    pushd $GIT_SERVE_ROOT/$name.git
     git --bare init --shared
-    ln -s $HOME/.githooks/post-receive /git/awe/$name.git/hooks/post-receive
+    ln -s $HOME/.githooks/post-receive $GIT_SERVE_ROOT/$name.git/hooks/post-receive
     git remote add github git@github.com:$GH_USERNAME/$name.git
     git remote add gitlab git@gitlab.com:$GL_USERNAME/$name.git
-    sudo chown -R git:git /git/awe/$name.git
+    sudo chown -R git:git $GIT_SERVE_ROOT/$name.git
     popd
   end
 end
--- fish/setup.fish
+++ fish/setup.fish
@@ -2,3 +2,4 @@ set -U EDITOR em
 set -U SSH_KEY_PATH ~/.ssh/rsa_id
 set -U GH_USERNAME aatxe
 set -U GL_USERNAME aatxe
+set -U GIT_SERVE_ROOT /git/awe
