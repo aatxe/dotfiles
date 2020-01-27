eec76f64563373308a3572e43df154c73974b998
Aaron Weiss <awe@pdgn.co>
2020-01-27 16:37:30 -0500

    install merlin, coq, and ppx_deriving

--- setup-extras.sh
+++ setup-extras.sh
@@ -3,3 +3,10 @@
 
 # Install Rust.
 curl https://sh.rustup.rs -sSf | sh
+
+# Install important OCaml dependencies.
+opam instal ppx_deriving merlin
+
+# Install Coq with opam.
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam install coq
