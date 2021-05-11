e03df5f4adeca8e916b24936152a1c6d510b2c3f
Aaron Weiss <aweiss@hey.com>
2021-05-10 22:34:52 -0400

    chemacs and doom installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,19 @@ ln -s $wd/git $HOME/.config/git
 # Set up ideavimrc.
 ln -s $wd/ideavimrc $HOME/.ideavimrc
 
+# Set up chemacs.
+git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+ln -s $wd/emacs-profiles.el $HOME/.emacs-profiles.el
+
+# Set up Doom Emacs.
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.doom
+~/.emacs.doom/bin/doom install
+
 # Set up spacemacs.
-git clone -b develop https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+git clone -b develop https://github.com/syl20bnr/spacemacs $HOME/.emacs.spacemacs
 ln -s $wd/spacemacs $HOME/.spacemacs
-ln -s $wd/layers/* $HOME/.emacs.d/private/
-ln -s $wd/packages/* $HOME/.emacs.d/private/
+ln -s $wd/layers/* $HOME/.emacs.spacemacs/private/
+ln -s $wd/packages/* $HOME/.emacs.spacemacs/private/
 
 # Set up atom.
 mkdir $HOME/.atom
