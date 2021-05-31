c071504c47eb5dba4f90cd142d68a058849bdefa
Aaron Weiss <aweiss@hey.com>
2021-05-31 13:23:26 -0400

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
+# homebrew
+eval (/opt/homebrew/bin/brew shellenv)
 # GPG_TTY for terminal pinentry
 set -x GPG_TTY (tty)
 # Add cargo to path.
 set -x PATH $HOME/.cargo/bin $PATH
 # Add .local/bin to path.
 set -x PATH $HOME/.local/bin $PATH
+# Add emacs to path.
+set -x PATH $HOME/.emacs.doom/bin $PATH
 set -g fish_user_paths "/usr/local/opt/zip/bin" $fish_user_paths
 # opam
 eval (opam env)
